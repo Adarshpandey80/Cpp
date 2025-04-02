640e6ac86a75f1c705657efa49a019a952bd9935
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-04-02 20:01:07 +0530

    first cpp commit

---
+++ 29day.cpp
@@ -0,0 +1,96 @@
+// # include <iostream>
+// using namespace std;
+// int main (){
+//     int x = 10;
+//     cout<<"the value of x is ="<<x<<endl;
+//     x+=10;
+//      cout<<"the value of x is ="<<x<<endl;
+//      x-=2;
+//       cout<<"the value of x is ="<<x<<endl;
+//       x/=2;
+//     cout<<"the value of x is ="<<x<<endl;
+//       x%=2;
+//     cout<<"the value of x is ="<<x<<endl;
+
+// }
+
+
+
+    //  31march----------------------------------
+
+
+    // #include<iostream>
+    // using namespace std;
+    // int main(){
+    //  int a=6 ,b=3 ,c;
+    //  c=a+b;
+    //  cout<<"the sum of a and b is \n"<<c<<endl;
+    //  c=a-b;
+    //  cout<<"the difference of a and b is \n"<<c<<endl;
+    //  c=a*b;
+    //  cout<<"the product of a and b is \n"<<c<<endl;
+    //  c=a/b;
+    //  cout<<"the division of a and b is \n "<<c<<endl;
+    //  c=a%b;
+    //  cout<<"the  remainder a and b is \n"<<c<<endl;
+
+    //   return 0;
+    // }
+
+
+    // #include <iostream>
+    // #include<cmath>
+    // using namespace std;
+    // int main(){
+      // int a=4,b=5 ,c,r=2 ,d=4,h=5;
+      // float p=3.14;
+      // c=1/2*(a*b);
+      // cout <<"the area of trangle is \n"<<c<<endl;
+      // c= a*b;
+      // cout<<"the area of rectangle is \n"<<c<<endl;
+      // c= p*r*r;
+      // cout<<"the area of circle is \n"<<c<<endl;
+      // c=a*a;
+      // cout<<"the area of square is \n"<<c<<endl;
+      // c=d*h;
+      // cout<<" the area of rhombus is \n"<<c<<endl;
+      // c= (a+b)/2*h;
+      // cout<<"the area of trapezium is \n"<<c<<endl;
+      // c= a*b*p;
+      // cout<<"thee area of ellips is \n"<<c<<endl;
+      //  c= 4*p*r*r;
+      //  cout<<"the area of sphere is \n"<<c<<endl;
+      //  c=b*h;
+      //  cout<<"the area of paralleliogram is \n"<<c<<endl;
+      //  float e =1.7; 
+      //    c = e/4*a*a;
+      //  cout<<"the area of equilteral triangle is \n"<<c<<endl;
+
+
+                  //roots -------------------
+
+    //   int b , a,c;
+    //cout<<b<<a<<c;
+    //cin>>b>>a>>c;
+    //   float r,e ,d;
+
+    //   d = b*b-4*a*c;
+      
+    //   r= (-b+sqrt(d))/2*a;
+    //   e= (-b-sqrt(d))/2*a;
+    //   cout<<"the roots of the equation are \n"<<r<<" and "<<e<<endl;
+
+    //  } 
+
+
+
+    //                1april---------
+
+      //              2 april-------------
+
+     #include<iostream>
+     using namespace std;
+     int main(){
+    
+
+     }
---
+++ frist.cpp
@@ -0,0 +1,16 @@
+# include <iostream> 
+using namespace std ;
+int main (){
+    cout<<"hello world";
+   const int x = 4;
+    //  x = 5;
+    cout<<x<<endl; 
+    cout<<sizeof(x)<<endl;
+    int a,c,b;
+    a = 10;
+    c = 20;
+    b = 30;
+    cout<<a<<c<<b;
+
+    return 0;
+}
