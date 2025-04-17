ec2a64cc3c5fd1b20f5d6619e9a112128e86dcf9
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-04-17 21:02:17 +0530

    pattern

--- pattern.cpp
+++ pattern.cpp
@@ -141,19 +141,19 @@
     
 // }
 
-#include<iostream>
-using namespace std;
-int main(){
-    for(int i= 1;i<=5;i++){
-        for(int j=1;j<=5;j++){
-            if((j<=5) and (j==i or j==5)) {
-                cout<<"* ";
-            }
-           else {
-                cout<<" ";
-            }
-        }
-        cout<<endl;
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i= 1;i<=5;i++){
+//         for(int j=1;j<=5;j++){
+//             if((j<=5) and (j==i or j==5)) {
+//                 cout<<"* ";
+//             }
+//            else {
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<endl;
+//     }
     
-}
+// }
