675f05bfbd04501ad7c51ae8ebca025ed4f31433
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-04-18 23:10:07 +0530

    Pattern

--- pattern.cpp
+++ pattern.cpp
@@ -141,16 +141,39 @@
     
 // }
 
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
 // #include<iostream>
 // using namespace std;
 // int main(){
 //     for(int i= 1;i<=5;i++){
 //         for(int j=1;j<=5;j++){
-//             if((j<=5) and (j==i or j==5)) {
+//             if(i==1 or j==1 or i==5 or j==5 or j==i or j==i or j==6-i ) {
 //                 cout<<"* ";
 //             }
 //            else {
-//                 cout<<" ";
+//                 cout<<"  ";
+//             }
+//         }
+//         cout<<endl;
+//     }
+    
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i= 1;i<=7;i++){
+//         for(int j=1;j<=7;j++){
+//             if(i==1 or j==1 or i==7 or j==7  or i==4 ) {
+//                 cout<<"* ";
+//             }
+//            else {
+//                 cout<<"  ";
 //             }
 //         }
 //         cout<<endl;
