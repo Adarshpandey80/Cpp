7e3dd74608e52d87ce811240a409b5b7f0b27f77
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-04-19 18:55:24 +0530

    string day 2

---
+++ string.cpp
@@ -0,0 +1,77 @@
+// #include<iostream>
+// using namespace std ;
+// int main(){
+//     // string str ;
+//     // cout<<"enter srting"<<endl;
+//     // getline(cin,str);
+//     // cout<<str<<endl;
+
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std ;
+// int main(){
+  
+//     string str = "HELLO";
+//     cout<<str.length()<<endl;
+//     cout<<"for loop method"<<endl;
+//     for(int i=0 ;i<str.length();i++){
+//         cout<<str[i]<<endl;
+//     }
+
+//     // for of loop 
+
+//     cout<<"for of method"<<endl;
+//     for(char j : str){
+//         cout<<j<<endl;
+//     }
+
+//     return 0;
+// }
+
+// addition of two string
+
+// #include<iostream>
+// using namespace std ;
+// int main(){
+//    string str = "hello";
+//    string str1 = "bhopal";
+//    cout<<str+str1<<endl;
+
+//     return 0;
+// }
+
+// compair  to two string
+
+// #include<iostream>
+// using namespace std ;
+// int main(){
+//    string str = "hello";
+//    string str1 = "bhopal";
+//    cout<<(str==str1)<<endl;
+//    cout<<str.compare(str1)<<endl;
+
+//     return 0;
+// }
+
+// wap to reverse string welcome
+
+#include<iostream>
+using namespace std ;
+int main(){
+  string str = "welcome";
+  string str1 = "";
+  for(int i=0;i<str.length();i++){
+    str1 =  str[i]+str1 ;
+    
+    }
+    cout<<str1<<endl;
+
+    return 0;
+}
+//wap to remove white space in my name is adarsh
+// wap to count vovel and consonent and white space in  the given sting above
+//chack the programe the given two string scilent and listen is anargm or not 
+//wap to reverse the string welcome bhopal in such a way tha welcome can be reverse and bhopal can be revers and bhopal comes frist and bhopal comes last
+// print the all the ascii from a giveb string smoll a to smoll z and capital a to capital z 
