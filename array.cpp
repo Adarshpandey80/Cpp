d6d26ac4461f3ce359a11139719e11f357baebd0
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-04-25 19:00:39 +0530

    2d array

--- array.cpp
+++ array.cpp
@@ -104,20 +104,68 @@
 
 // wap to find consonent and vovel from array;
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   int arr[5] = {55,33,77,55,44};
+//   int vovel = 0;
+//   int conso = 0;
+//   for(int i =0 ;i<5;i++){
+//     if(arr[i]=='a'|| arr[i]=='e' || arr[i] == 'i' || arr[i]=='o' ||arr[i] == 'u'){
+//        vovel+=1;
+        
+//     } else {
+//         conso+=1;
+//     }
+//   }
+//      cout<<vovel<<endl;
+//      cout<<conso<<endl;
+// }
+
+
+//                 25/04/25 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   cout<<"enter the size of 2 d array"<<endl;
+//   int n;
+//   cin>>n;
+//   int arr[n][n];
+//   int count =1;
+//   for(int i=0 ; i<n;i++ ){
+//     for(int j=0;j<n;j++){
+//       cout<<"enter the elements "<<count<<endl;
+//       cin>>arr[i][j];
+//       count++;
+
+//     }
+//   }
+//   for(int i1=0 ; i1<n;i1++ ){
+//     for(int j1=0;j1<n;j1++){
+     
+//       cout<<arr[i1][j1];
+//     }
+//   cout<<endl;
+//   }
+
+
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-  int arr[5] = {55,33,77,55,44};
-  int vovel = 0;
-  int conso = 0;
-  for(int i =0 ;i<5;i++){
-    if(arr[i]=='a'|| arr[i]=='e' || arr[i] == 'i' || arr[i]=='o' ||arr[i] == 'u'){
-       vovel+=1;
-        
+  int arr[6] = {22,44,55,66,77,88};
+  for(int i = 1 ;i<=6;i++){
+    cout<<"enter your choice"<<i<<endl;
+    int ch;
+    cin>>ch;
+    int j = rand()%6;
+    cout<<j<<endl;
+    if(ch == arr[j]){
+        cout<<"you are correct"<<i<<endl;
     } else {
-        conso+=1;
+        cout<<"you are wrong"<<i<<endl;
     }
   }
-     cout<<vovel<<endl;
-     cout<<conso<<endl;
 }
