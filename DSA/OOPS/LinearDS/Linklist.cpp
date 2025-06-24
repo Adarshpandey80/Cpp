5def0a350642b9d059889be05cf54e50a3715f45
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-06-24 19:52:09 +0530

    DoublyLinkList

--- DSA/OOPS/LinearDS/Linklist.cpp
+++ DSA/OOPS/LinearDS/Linklist.cpp
@@ -32,7 +32,99 @@
 //     return 0;
 // }
 
-// =========================================Add node at first and last in singly linked list========================================
+// =========================================Add node at first and last , specific position and delte node singly linked list========================================
+
+
+// #include<iostream>
+// using namespace std;
+// class Node{
+//     public:
+//         int data;
+//         Node *next;
+//     Node(int d){
+//         data=d;
+//         next=NULL;
+//     }
+//     void display(Node *head){
+//         Node *temp=head;
+//         while(temp!=NULL){
+//             cout<<"===>"<<temp->data;//10==>20==>30==>40==>
+//             temp=temp->next;
+//         }
+//     }
+   
+//     Node * addFirstNode(Node *head,int d){
+//         Node *newNode=new Node(d);
+//         newNode->next=head;
+//         head=newNode;
+//         return head;
+//     }
+   
+//     void addLastNode(Node *head,int d){
+//         Node *newNode=new Node(d);
+//         Node *temp=head;
+//         while(temp->next!=NULL){
+//             temp=temp->next;
+//         }
+//         temp->next=newNode;
+//     }
+//     //Node delete
+//     Node * deleteFirst(Node *head){
+//         Node *temp=head;
+//         head=head->next;
+//         delete(temp);
+//         return head;
+//     }
+   
+//     void deleteLast(Node *head){
+//         Node *temp=head;
+//         while(temp->next->next!=NULL){
+//             temp=temp->next;
+//         }
+//         Node *temp2=temp->next;
+//         temp->next=NULL;
+//         delete(temp2);
+//     }
+   
+//     void deleteAtPos(Node *head,int pos){
+//         Node *temp1=head;
+//         Node *temp2=head->next;
+//         pos--;//2
+//         while(pos>1){
+//             temp1=temp1->next;
+//             temp2=temp2->next;
+//             pos--;//1
+//         }
+//         Node *temp3=temp2;
+//         temp1->next=temp2->next;
+//         delete(temp3);
+       
+//     }
+// };
+
+// int main(){
+//     Node *first=new Node(10);
+//     Node *second=new Node(20);
+//     Node *third=new Node(30);
+//     Node *forth=new Node(40);
+//     first->next=second;
+//     first->next->next=third;
+//     first->next->next->next=forth;
+//     Node *head=first;
+//     first->display(head);
+// //    head=head->deleteFirst(head);
+//     cout<<"\nData of SIngly Linked List after Delete first Node : \n";
+//     head->display(head);
+// //    head->deleteLast(head);
+//     cout<<"\nData of SIngly Linked List after Delete first Node : \n";
+//     head->display(head);
+//     head->deleteAtPos(head,3);
+//     cout<<"\nData of SIngly Linked List after Delete Node 3 : \n";
+//     head->display(head);
+// return 0;
+// }
+
+// =========================================Add node at position linked list========================================
 
 // #include<iostream>
 // using namespace std;
@@ -67,6 +159,20 @@
 // 		}
 // 		temp->next=newNode;
 // 	}
+	
+// 	void addNodeAtPos(Node *head,int data,int pos){
+// 		Node *newNode=new Node(data);
+// 		Node *temp1=head;
+// 		Node *temp2=head->next;
+// 		pos--;
+// 		while(pos>1){
+// 			temp1=temp1->next;
+// 			temp2=temp2->next;
+// 			pos--;
+// 		}
+// 		temp1->next=newNode;
+// 		newNode->next=temp2;
+// 	}
 // };
 
 // int main(){
@@ -79,78 +185,11 @@
 // 	first->next->next->next=forth;
 // 	Node *head=first;
 // 	first->display(head);
-// 	head=head->addFirstNode(head,5);
-// 	cout<<"\n Data of Singly Linked List After Add New Node at Starting \n";
-// 	head->display(head);
-// 	head->addLastNode(head,50);
-// 	cout<<"\n Data of Singly Linked List After Add New Node at Ending \n";
-// 	head->display(head);
+// 	head->addNodeAtPos(head,35,4);
+// 	cout<<"\n after add new Node at specific position \n";
+// 	first->display(head);
 // 	return 0;
 // }
 
-// =========================================Add node at position linked list========================================
+// ==========================================23/6/25 Deletion of linklist==================
 
-#include<iostream>
-using namespace std;
-class Node{
-	public:
-		int data;
-		Node *next;
-	Node(int d){
-		data=d;
-		next=NULL;
-	}
-	void display(Node *head){
-		Node *temp=head;
-		while(temp!=NULL){
-			cout<<"===>"<<temp->data;//10==>20==>30==>40==>
-			temp=temp->next;
-		}
-	}
-	
-	Node * addFirstNode(Node *head,int d){
-		Node *newNode=new Node(d);
-		newNode->next=head;
-		head=newNode;
-		return head;
-	}
-	
-	void addLastNode(Node *head,int d){
-		Node *newNode=new Node(d);
-		Node *temp=head;
-		while(temp->next!=NULL){
-			temp=temp->next;
-		}
-		temp->next=newNode;
-	}
-	
-	void addNodeAtPos(Node *head,int data,int pos){
-		Node *newNode=new Node(data);
-		Node *temp1=head;
-		Node *temp2=head->next;
-		pos--;
-		while(pos>1){
-			temp1=temp1->next;
-			temp2=temp2->next;
-			pos--;
-		}
-		temp1->next=newNode;
-		newNode->next=temp2;
-	}
-};
-
-int main(){
-	Node *first=new Node(10);
-	Node *second=new Node(20);
-	Node *third=new Node(30);
-	Node *forth=new Node(40);
-	first->next=second;
-	first->next->next=third;
-	first->next->next->next=forth;
-	Node *head=first;
-	first->display(head);
-	head->addNodeAtPos(head,35,4);
-	cout<<"\n after add new Node at specific position \n";
-	first->display(head);
-	return 0;
-}
