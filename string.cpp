0a28131fced41f8ac5207c97bac2d1afcc28524f
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-08-22 23:22:57 +0530

    string questions

--- string.cpp
+++ string.cpp
@@ -198,3 +198,62 @@
 //     cout << str << endl;
    
 // }
+
+// Permutation in string 
+
+// class Solution {
+//     bool isFreqSame (int freq1[],int freq2[]){
+//         for(int i=0;i<26;i++){
+//             if(freq1[i] != freq2[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// public:
+//     bool checkInclusion(string s1, string s2) {
+//         int freq[26] = {0};
+//         for(int i=0; i<s1.length();i++){
+//             freq[s1[i]-'a']++;
+//         }
+
+//         int windSize = s1.length();
+//         for(int i=0;i<s2.length();i++){
+//             int windIdx = 0,idx=i;
+//             int windFreq[26] ={0};
+
+//             while(windIdx<windSize && idx<s2.length()){
+//                 windFreq[s2[idx]-'a']++;
+//                 windIdx++ ; idx++;
+//             }
+//             if(isFreqSame(freq,windFreq)){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+ int compress(vector<char>& chars) {
+        int n = chars.size();
+        int idx = 0;
+        for(int i=0;i<n;i++){
+            if(chars[i]!=chars[idx]){
+                idx++;
+                chars[idx] = chars[i];
+            }
+        }
+        
+        for(char st :  chars){
+            cout<<st<<" ";
+        }
+    }
+
+    int main (){
+        vector<char> chars = {'a','a','c','c','d'};
+        compress(chars);
+    }
