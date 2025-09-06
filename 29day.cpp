ed0d2d576ed3852073b8e65993261ac5e2c0af35
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-09-06 22:44:46 +0530

    Rate in Maze Problem

--- 29day.cpp
+++ 29day.cpp
@@ -865,3 +865,45 @@
 // }
 
 //                   18/04/25
+
+// Rate in a Maze Problem
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void helper(vector<vector<int>> &mat , int r, int c, string path, vector<string> &ans , vector<vector<bool>> &vis){
+     int n = mat.size();
+     if(r<0 || c<0 || r>=n || c>=n || vis[r][c] == true || mat[r][c] == 0){
+          return;
+     }
+     if(r == n-1 && c == n-1){
+          ans.push_back(path);
+          return;
+     }
+     vis[r][c] = true;
+     helper(mat , r-1 , c , path + 'U' , ans , vis);
+     helper(mat , r+1 , c , path + 'D' , ans , vis);
+     helper(mat , r , c-1 , path + 'L' , ans , vis);
+     helper(mat , r , c+1 , path + 'R' , ans , vis);
+     vis[r][c] = false;
+}
+// complete this function
+vector<string> findPath(vector<vector<int>> &mat) {
+     int n = mat.size();
+     vector<string> ans;
+     string path = "";
+     vector<vector<bool>> vis(n , vector<bool>(n , false));
+     helper(mat , 0 , 0 , path , ans , vis);
+     return ans;
+}
+
+int main(){
+     vector<vector<int>> mat = {{1,0,0,0},{1,1,0,1},{1,1,0,0},{0,1,1,1}};
+     int n = mat.size();
+     vector<string> ans = findPath(mat );
+     for(auto i : ans){
+          cout<<i<<endl;
+     }
+     return 0;
+}
