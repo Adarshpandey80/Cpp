18c54fc2902e1e0bfa1b8bdb50fef9ca67dd9a8e
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-09-13 20:54:13 +0530

    array problem

--- 29day.cpp
+++ 29day.cpp
@@ -909,54 +909,73 @@
 // }
 
 // palindrome partioning 
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+
+// class Solution {
+// public:
+//    bool isPalion(string s){
+//     string s2 = s;
+//     reverse(s2.begin() , s2.end());
+//     return s == s2;
+//    }
+
+
+//    void getAllParts(string s ,vector<string>  &partitions ,vector<vector<string>> &ans){
+//     if(s.size()==0){
+//         ans.push_back(partitions);
+//         return ;
+//     }
+//     for(int i=0; i<s.size(); i++){
+//         string part = s.substr(0,i+1);
+
+//         if(isPalion(part)){
+//             partitions.push_back(part);
+//             getAllParts(s.substr(i+1) , partitions , ans);
+//             partitions.pop_back();
+//         }
+//     }
+//    }
+
+
+//     vector<vector<string>> partition(string s) {
+//         vector<vector<string>> ans;
+//         vector<string>  partitions ;
+//         getAllParts(s, partitions , ans);
+//         return ans;
+//     }
+// };
+
+// int main(){
+//     Solution obj;
+//     string s = "aab";
+//     vector<vector<string>> ans = obj.partition(s);
+//     for(auto i : ans){
+//         for(auto j : i){
+//             cout<<j<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
-
-class Solution {
-public:
-   bool isPalion(string s){
-    string s2 = s;
-    reverse(s2.begin() , s2.end());
-    return s == s2;
-   }
-
-
-   void getAllParts(string s ,vector<string>  &partitions ,vector<vector<string>> &ans){
-    if(s.size()==0){
-        ans.push_back(partitions);
-        return ;
-    }
-    for(int i=0; i<s.size(); i++){
-        string part = s.substr(0,i+1);
-
-        if(isPalion(part)){
-            partitions.push_back(part);
-            getAllParts(s.substr(i+1) , partitions , ans);
-            partitions.pop_back();
-        }
-    }
-   }
-
-
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string>  partitions ;
-        getAllParts(s, partitions , ans);
-        return ans;
-    }
-};
-
 int main(){
-    Solution obj;
-    string s = "aab";
-    vector<vector<string>> ans = obj.partition(s);
-    for(auto i : ans){
-        for(auto j : i){
-            cout<<j<<" ";
-        }
-        cout<<endl;
+    int arr[7] = {3,4,7,6,7,2,2};
+    int x =6 ;
+    int count=0;
+    for(int i=0 ;i<=7;i++){
+      if(arr[i]<=x){
+        count++;
+  
+      } else {
+        break;
+      }
     }
-    return 0;
+
+   cout<<count;
+    
 }
