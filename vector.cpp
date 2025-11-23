0276d695fab976aded12050b915a6833b8318cc3
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-11-23 18:00:20 +0530

    productExceptSelf

--- vector.cpp
+++ vector.cpp
@@ -221,15 +221,37 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int j = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             if(nums[i] != 0){
+//                 swap(nums[i], nums[j]);
+//                 j++;
+//             }
+//         }
+//     }
+// };
+
+
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int j = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                swap(nums[i], nums[j]);
-                j++;
-            }
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        int prefix = 1;
+        for(int i = 0; i < n; i++){
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
+
+        int suffix = 1;
+        for(int i = n-1; i >= 0; i--){
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return ans;
     }
 };
