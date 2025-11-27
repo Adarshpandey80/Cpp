5694a4648b19f58c2937919e5c8474bf1d62f574
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-11-27 19:47:43 +0530

    merge sorted array

--- vector.cpp
+++ vector.cpp
@@ -286,12 +286,28 @@
 
 
 
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         k %= nums.size();
+//         reverse(nums.begin(), nums.end());
+//         reverse(nums.begin(), nums.begin() + k);
+//         reverse(nums.begin() + k, nums.end());
+//     }
+// };
+
+
+
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        k %= nums.size();
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+
+        while(j >= 0){
+            if(i >= 0 && A[i] > B[j])
+                A[k--] = A[i--];
+            else
+                A[k--] = B[j--];
+        }
     }
 };
