16a72d1100f84dbc0e40b985878f7da52eb83eb6
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-11-30 18:27:06 +0530

    longestConsecutive

--- vector.cpp
+++ vector.cpp
@@ -325,6 +325,25 @@
 // };
 
 
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         unordered_set<int> s(nums.begin(), nums.end());
+//         int longest = 0;
+
+//         for(int n : s){
+//             if(!s.count(n - 1)){  // start of sequence
+//                 int len = 1;
+//                 while(s.count(n + len)) len++;
+//                 longest = max(longest, len);
+//             }
+//         }
+//         return longest;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
