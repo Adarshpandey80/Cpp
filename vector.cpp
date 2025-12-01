7d582baa1f9f6933711c66737f12ed6407249dbe
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-12-01 20:18:24 +0530

    3sum

--- vector.cpp
+++ vector.cpp
@@ -344,19 +344,47 @@
 
 
 
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& nums) {
+//         unordered_set<int> s(nums.begin(), nums.end());
+//         int longest = 0;
+
+//         for(int n : s){
+//             if(!s.count(n - 1)){  // start of sequence
+//                 int len = 1;
+//                 while(s.count(n + len)) len++;
+//                 longest = max(longest, len);
+//             }
+//         }
+//         return longest;
+//     }
+// };
+
+
 class Solution {
 public:
-    int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(), nums.end());
-        int longest = 0;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
 
-        for(int n : s){
-            if(!s.count(n - 1)){  // start of sequence
-                int len = 1;
-                while(s.count(n + len)) len++;
-                longest = max(longest, len);
+        for(int i = 0; i < nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int l = i + 1, r = nums.size() - 1;
+
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum < 0) l++;
+                else if(sum > 0) r--;
+                else{
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
+                    l++; r--;
+                }
             }
         }
-        return longest;
+        return ans;
     }
 };
