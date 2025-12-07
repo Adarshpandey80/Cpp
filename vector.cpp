5f6a37e17d179aa5e81a59e4c708ebc44d420749
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-12-07 20:47:07 +0530

    findDuplicate

--- vector.cpp
+++ vector.cpp
@@ -498,26 +498,44 @@
 // };
 
 
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int l = 0, r = nums.size() - 1;
+
+//         while(l <= r){
+//             int mid = (l + r) / 2;
+
+//             if(nums[mid] == target) return mid;
+
+//             // Left part sorted
+//             if(nums[l] <= nums[mid]){
+//                 if(nums[l] <= target && target < nums[mid]) r = mid - 1;
+//                 else l = mid + 1;
+//             }
+//             else{ // right part sorted
+//                 if(nums[mid] < target && target <= nums[r]) l = mid + 1;
+//                 else r = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
 
-        while(l <= r){
-            int mid = (l + r) / 2;
-
-            if(nums[mid] == target) return mid;
-
-            // Left part sorted
-            if(nums[l] <= nums[mid]){
-                if(nums[l] <= target && target < nums[mid]) r = mid - 1;
-                else l = mid + 1;
-            }
-            else{ // right part sorted
-                if(nums[mid] < target && target <= nums[r]) l = mid + 1;
-                else r = mid - 1;
-            }
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
 };
