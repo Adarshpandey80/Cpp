5a70cc071f9caf82f8bcb37587355b2df8e7825a
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-12-10 23:13:09 +0530

    findKthLargest

--- vector.cpp
+++ vector.cpp
@@ -540,16 +540,25 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int maxArea(vector<int>& h) {
+//         int l = 0, r = h.size() - 1, ans = 0;
+
+//         while(l < r){
+//             ans = max(ans, min(h[l], h[r]) * (r - l));
+//             if(h[l] < h[r]) l++;
+//             else r--;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int maxArea(vector<int>& h) {
-        int l = 0, r = h.size() - 1, ans = 0;
-
-        while(l < r){
-            ans = max(ans, min(h[l], h[r]) * (r - l));
-            if(h[l] < h[r]) l++;
-            else r--;
-        }
-        return ans;
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        while(--k) pq.pop();
+        return pq.top();
     }
 };
