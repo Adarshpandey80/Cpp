0aead18541b4461ad6e7e471f537f42d29bb588f
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-12-11 22:09:15 +0530

    subarraySum

--- vector.cpp
+++ vector.cpp
@@ -554,11 +554,27 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq(nums.begin(), nums.end());
+//         while(--k) pq.pop();
+//         return pq.top();
+//     }
+// };
+
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(), nums.end());
-        while(--k) pq.pop();
-        return pq.top();
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0, count = 0;
+
+        for(int n : nums){
+            sum += n;
+            if(mp.count(sum - k)) count += mp[sum - k];
+            mp[sum]++;
+        }
+        return count;
     }
 };
