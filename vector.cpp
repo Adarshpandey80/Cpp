c3604a75bb1d4cf12cfbf7fbe3bdd2e1fe9726cf
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-12-19 22:46:05 +0530

    findKthLargest

--- vector.cpp
+++ vector.cpp
@@ -612,24 +612,34 @@
 // };
 
 
+// class Solution {
+// public:
+//     int trap(vector<int>& h) {
+//         int l = 0, r = h.size() - 1;
+//         int leftMax = 0, rightMax = 0, water = 0;
+
+//         while(l < r){
+//             if(h[l] < h[r]){
+//                 if(h[l] >= leftMax) leftMax = h[l];
+//                 else water += leftMax - h[l];
+//                 l++;
+//             }
+//             else{
+//                 if(h[r] >= rightMax) rightMax = h[r];
+//                 else water += rightMax - h[r];
+//                 r--;
+//             }
+//         }
+//         return water;
+//     }
+// };
+
+
 class Solution {
 public:
-    int trap(vector<int>& h) {
-        int l = 0, r = h.size() - 1;
-        int leftMax = 0, rightMax = 0, water = 0;
-
-        while(l < r){
-            if(h[l] < h[r]){
-                if(h[l] >= leftMax) leftMax = h[l];
-                else water += leftMax - h[l];
-                l++;
-            }
-            else{
-                if(h[r] >= rightMax) rightMax = h[r];
-                else water += rightMax - h[r];
-                r--;
-            }
-        }
-        return water;
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        while(--k) pq.pop();
+        return pq.top();
     }
 };
