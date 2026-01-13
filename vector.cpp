a7c4244f315d268b41282b3c55c5f6cb71d13d85
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-01-13 22:12:10 +0530

    findPeakElement

--- vector.cpp
+++ vector.cpp
@@ -635,11 +635,24 @@
 // };
 
 
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq(nums.begin(), nums.end());
+//         while(--k) pq.pop();
+//         return pq.top();
+//     }
+// };
+
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(), nums.end());
-        while(--k) pq.pop();
-        return pq.top();
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while(l < r){
+            int mid = (l + r) / 2;
+            if(nums[mid] > nums[mid + 1]) r = mid;
+            else l = mid + 1;
+        }
+        return l;
     }
 };
