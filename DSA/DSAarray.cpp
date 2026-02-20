59eae29f1e07f63bc1df7dae2accab02809b0587
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-02-20 23:28:10 +0530

    2 sum optimal solution

--- DSA/DSAarray.cpp
+++ DSA/DSAarray.cpp
@@ -334,22 +334,59 @@
 
 // ====================# 3 sum ==========================
 
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+
+// using namespace std;
+// int main(){
+//     vector<int> arr = {-1,0,1,2,-1,-4};
+ 
+//     int n = arr.size();
+//     for(int i=0;i<n; i++){
+//         for(int j=i+1;j<n;j++){
+//             for(int k= j+1 ; k<n;k++){
+//               if(arr[i]+arr[j]+arr[k] == 0){
+//                 cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<endl;
+//               }
+//             }
+//         }
+//     }
+// }
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
 using namespace std;
 int main(){
     vector<int> arr = {-1,0,1,2,-1,-4};
- 
+    vector<vector<int>> ans;
+    sort(arr.begin() , arr.end());
     int n = arr.size();
-    for(int i=0;i<n; i++){
-        for(int j=i+1;j<n;j++){
-            for(int k= j+1 ; k<n;k++){
-              if(arr[i]+arr[j]+arr[k] == 0){
-                cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k]<<endl;
-              }
-            }
+    for(int i=0;i<n;i++){
+        if(i>0 && arr[i] == arr[i-1] ) continue;
+        int j= i + 1;
+        int k = n - 1;
+
+     while(j<k){
+        int sum = arr[i] + arr[j] + arr[k];
+        if(sum<0){
+          j++;
+        } else if(sum>0){
+         k--;
+        } else {
+          vector<int> temp = {arr[i], arr[j] , arr[k]};
+          ans.push_back(temp);
+          j++;
+          k--;
+          while(j < k && arr[j]==arr[j-1]) j++; 
+          while(j < k && arr[k]==arr[k+1]) k--; 
         }
+     }
+         
     }
+   for(auto it : ans){
+      cout<<it[0]<<" "<<it[1]<<" "<<it[2]<<endl;
+   }
 }
