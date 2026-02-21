6b108cf5ac11b382a3c92ec0f9dd7e91f7125188
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-02-21 23:06:24 +0530

    merge two sorted array without using extra space

--- DSA/DSAarray.cpp
+++ DSA/DSAarray.cpp
@@ -450,3 +450,74 @@
 //       }
 //       cout<<count;
 // }
+
+
+// =====================merce 2 sorted array without extra space============
+
+// #include<iostream>
+// #include<vector>
+
+// using namespace std;
+// int main(){
+//     vector<int> arr1 = {1,3,5,7};
+//     vector<int> arr2 = {0,2,6,8,9};
+//     vector<int> ans; 
+//     int frist = 0;
+//     int second = 0 ;
+//     int n1 = arr1.size();
+//     int n2 = arr2.size();
+//     while(frist<n1 && second<n2){
+//         if(arr1[frist] < arr2[second]){
+//             ans.push_back(arr1[frist]);
+//             frist++;
+//         } else {
+//        ans.push_back(arr2[second]);
+//        second ++;
+//         }
+//     }
+
+//     while(frist<n1){
+//         ans.push_back(arr1[frist]);
+//         frist++;
+//     }
+//     while(second<n2){
+//         ans.push_back(arr2[second]);
+//         second++;
+//     }
+//    for(auto it : ans){
+//     cout<<it<<" ";
+//    }
+// }
+
+// =====================merce 2 sorted array without extra space(optimal)============
+
+#include<iostream>
+#include <algorithm>
+using namespace std;
+int main(){
+    int arr1[] = {1,3,5,7};
+      int arr2[] = {0,2,6,8,9};
+      int n = 4;
+      int n2 = 5;
+      int left = n-1;
+      int right = 0;
+   while(left>=0 && right < n2){
+       if(arr1[left] > arr2[right]){
+        swap(arr1[left] , arr2[right]);
+           left--;
+       right++;
+       } else {
+        break;
+       }
+    
+   }
+   sort(arr1, arr1+n);
+   sort(arr2 , arr2+n2);
+
+   for(int i = 0;i<n;i++){
+    cout<<arr1[i]<<" ";
+   }
+   for(int i = 0;i<n2;i++){
+    cout<<arr2[i]<<" ";
+   }
+}
