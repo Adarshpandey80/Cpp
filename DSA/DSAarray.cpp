7406946f75e3062cb585283e0e2a0765b8914ca6
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-02-22 16:45:23 +0530

    reverse pairs

--- DSA/DSAarray.cpp
+++ DSA/DSAarray.cpp
@@ -491,33 +491,51 @@
 
 // =====================merce 2 sorted array without extra space(optimal)============
 
+// #include<iostream>
+// #include <algorithm>
+// using namespace std;
+// int main(){
+//     int arr1[] = {1,3,5,7};
+//       int arr2[] = {0,2,6,8,9};
+//       int n = 4;
+//       int n2 = 5;
+//       int left = n-1;
+//       int right = 0;
+//    while(left>=0 && right < n2){
+//        if(arr1[left] > arr2[right]){
+//         swap(arr1[left] , arr2[right]);
+//            left--;
+//        right++;
+//        } else {
+//         break;
+//        }
+    
+//    }
+//    sort(arr1, arr1+n);
+//    sort(arr2 , arr2+n2);
+
+//    for(int i = 0;i<n;i++){
+//     cout<<arr1[i]<<" ";
+//    }
+//    for(int i = 0;i<n2;i++){
+//     cout<<arr2[i]<<" ";
+//    }
+// }
+
+
 #include<iostream>
-#include <algorithm>
+#include<vector>
 using namespace std;
 int main(){
-    int arr1[] = {1,3,5,7};
-      int arr2[] = {0,2,6,8,9};
-      int n = 4;
-      int n2 = 5;
-      int left = n-1;
-      int right = 0;
-   while(left>=0 && right < n2){
-       if(arr1[left] > arr2[right]){
-        swap(arr1[left] , arr2[right]);
-           left--;
-       right++;
-       } else {
-        break;
-       }
-    
-   }
-   sort(arr1, arr1+n);
-   sort(arr2 , arr2+n2);
-
-   for(int i = 0;i<n;i++){
-    cout<<arr1[i]<<" ";
-   }
-   for(int i = 0;i<n2;i++){
-    cout<<arr2[i]<<" ";
-   }
+    vector<int> arr = {40,25,19,12,9,6,2};
+    int n = arr.size();
+    int count = 0;
+    for(int i= 0;i<n;i++){
+        for(int j=i+1 ; j<n;j++){
+            if(arr[i]>2*(arr[j])){
+                count++;
+            }
+        }
+    }
+    cout<<count;
 }
