9bfdf6c435aebd86683b39ebf61605538e269a72
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-03-02 23:05:31 +0530

    find pair with given sum in DLL , Remove Duplication from a sorted DLL

--- DSA/DoublyLinkList.cpp
+++ DSA/DoublyLinkList.cpp
@@ -1,90 +1,112 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
-    Node* back;
-    public:
-    Node(int data1 , Node* next1, Node* back1){
+    Node *next;
+    Node *back;
+
+public:
+    Node(int data1, Node *next1, Node *back1)
+    {
         data = data1;
         next = next1;
         back = back1;
     }
-    public:
-    Node(int data1){
+
+public:
+    Node(int data1)
+    {
         data = data1;
         next = nullptr;
         back = nullptr;
     }
 };
 
-Node* convertArray2DLL(vector<int> & arr){
-   Node* head = new Node(arr[0]);
-   Node* mover = head;
-   for(int i=1;i<arr.size();i++){
-   Node* temp = new Node(arr[i]);
-    mover->next = temp;
-    temp->back = mover;
-    mover = temp;
-   }
-   return head;
-}
-
-void printDLL(Node* head){
-    while(head != NULL){
-         cout<<head->data<<" "; 
-         head = head->next;
+Node *convertArray2DLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        temp->back = mover;
+        mover = temp;
     }
-}
-
-Node* deletehead(Node* head){
-    if(head==NULL) return NULL;
-    Node* temp = head;
-    head = head->next;
-    delete(temp);
     return head;
 }
 
-Node* deletetail(Node* head){
-     if(head==NULL) return NULL;
-     if(head->next ==NULL){
-        delete(head);
+void printDLL(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+Node *deletehead(Node *head)
+{
+    if (head == NULL)
         return NULL;
-     }
-    Node* temp = head;
-    while(temp->next != NULL){
+    Node *temp = head;
+    head = head->next;
+    delete (temp);
+    return head;
+}
+
+Node *deletetail(Node *head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL)
+    {
+        delete (head);
+        return NULL;
+    }
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
-    Node* prev = temp->back;
+    Node *prev = temp->back;
     prev->next = nullptr;
     temp->back = nullptr;
-    delete(temp);
+    delete (temp);
     return head;
 }
 
-Node* deletekthelement(Node* head , int k){
-    if(head==NULL) return NULL;
+Node *deletekthelement(Node *head, int k)
+{
+    if (head == NULL)
+        return NULL;
     int cnt = 0;
-    Node* temp = head;
-    while(temp != NULL){
+    Node *temp = head;
+    while (temp != NULL)
+    {
         cnt++;
-        if(cnt == k)  break;
-        
-        temp = temp->next;
-    } 
-    Node* prev = temp->back;
-    Node* front = temp->next;
+        if (cnt == k)
+            break;
 
-    if(prev == NULL && front==NULL){
+        temp = temp->next;
+    }
+    Node *prev = temp->back;
+    Node *front = temp->next;
+
+    if (prev == NULL && front == NULL)
+    {
         return NULL;
     }
-    if(prev == NULL){
+    if (prev == NULL)
+    {
         return deletehead(head);
     }
-    if(front == NULL){
+    if (front == NULL)
+    {
         return deletetail(head);
     }
     prev->next = front;
@@ -97,83 +119,94 @@ Node* deletekthelement(Node* head , int k){
 
 // ====================insert in DLL =============================
 
-Node* inserthead(Node* head , int val){
+Node *inserthead(Node *head, int val)
+{
 
-//  Node* newhead = new Node(val);  // manual set 
-//  newhead->next = head;
-//   head->back = newhead; 
-//   return newhead;
-   
-    Node* newhead = new Node(val , head , nullptr); // constructor use 
+    //  Node* newhead = new Node(val);  // manual set
+    //  newhead->next = head;
+    //   head->back = newhead;
+    //   return newhead;
+
+    Node *newhead = new Node(val, head, nullptr); // constructor use
     head->back = newhead;
     return newhead;
 }
 
-Node* insertail(Node* head , int val){
-    if(head == NULL) {
-        return inserthead(head , val);
+Node *insertail(Node *head, int val)
+{
+    if (head == NULL)
+    {
+        return inserthead(head, val);
     }
-  
-    Node* temp = head;
-    while(temp->next != NULL){
-          temp = temp->next;
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
     }
-    Node* newNode = new Node(val);
+    Node *newNode = new Node(val);
     newNode->back = temp;
     temp->next = newNode;
     return head;
 }
 
-Node* insertbeforetail(Node* head , int val){
-     if(head == NULL) {
-        return inserthead(head , val);
+Node *insertbeforetail(Node *head, int val)
+{
+    if (head == NULL)
+    {
+        return inserthead(head, val);
     }
-  
-    Node* temp = head;
-    while(temp->next != NULL){
-          temp = temp->next;
-    }
-   Node* prev = temp->back;
-   Node* newNode = new Node (val , temp , prev);
-   prev->next = newNode;
-   temp->back = newNode;
-    return head;
-}
 
-Node* insertkthelemet(Node* head , int k , int val){
-    if(k==1){
-        return inserthead(head , val);
-    }
-    int cnt = 0;
-    Node* temp = head;
-    while(temp!=NULL){
-        cnt++;
-        if(cnt==k) break;
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
-    Node* prev = temp->back;
-    Node* newNode = new Node(val , temp , prev);
+    Node *prev = temp->back;
+    Node *newNode = new Node(val, temp, prev);
     prev->next = newNode;
     temp->back = newNode;
     return head;
-
 }
 
+Node *insertkthelemet(Node *head, int k, int val)
+{
+    if (k == 1)
+    {
+        return inserthead(head, val);
+    }
+    int cnt = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cnt++;
+        if (cnt == k)
+            break;
+        temp = temp->next;
+    }
+    Node *prev = temp->back;
+    Node *newNode = new Node(val, temp, prev);
+    prev->next = newNode;
+    temp->back = newNode;
+    return head;
+}
 
 // =====================================DSA Problems=================
 
+// reverse DLL;
 
-//reverse DLL;
-
-Node* reverseDLL(Node* head){
+Node *reverseDLL(Node *head)
+{
     stack<int> st;
-    Node* temp = head;
-    while(temp!=NULL){
+    Node *temp = head;
+    while (temp != NULL)
+    {
         st.push(temp->data);
         temp = temp->next;
     }
     temp = head;
-    while(temp!=NULL){
+    while (temp != NULL)
+    {
         temp->data = st.top();
         st.pop();
         temp = temp->next;
@@ -181,16 +214,18 @@ Node* reverseDLL(Node* head){
     return head;
 }
 
-////===================method 2 
-Node* reverseDLL2(Node* head){
+////===================method 2
+Node *reverseDLL2(Node *head)
+{
 
-    if(head == NULL || head->next == NULL)
+    if (head == NULL || head->next == NULL)
         return head;
 
-    Node* temp = NULL;
-    Node* current = head;
+    Node *temp = NULL;
+    Node *current = head;
 
-    while(current != NULL){
+    while (current != NULL)
+    {
         // Swap next and back
         temp = current->back;
         current->back = current->next;
@@ -201,41 +236,105 @@ Node* reverseDLL2(Node* head){
     }
 
     // After loop, temp is pointing to node before NULL
-    if(temp != NULL)
+    if (temp != NULL)
         head = temp->back;
 
     return head;
 }
 
-
 // Delete all occurance key
 
-Node* deleteoccurance(Node* head, int key){
-    Node* temp = head;
-    while(temp!=NULL){
-        if(temp->data == key){
-            if(temp == head){
+Node *deleteoccurance(Node *head, int key)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        if (temp->data == key)
+        {
+            if (temp == head)
+            {
                 head = temp->next;
             }
-            Node* prevNode = temp->back;
-            Node* nextNode = temp->next;
-            if(nextNode!=NULL) nextNode->back = prevNode;
-            if(prevNode!= NULL) prevNode->next = nextNode;
-            delete(temp);
+            Node *prevNode = temp->back;
+            Node *nextNode = temp->next;
+            if (nextNode != NULL)
+                nextNode->back = prevNode;
+            if (prevNode != NULL)
+                prevNode->next = nextNode;
+            delete (temp);
             temp = nextNode;
-        } else {
-            temp= temp->next;
+        }
+        else
+        {
+            temp = temp->next;
         }
     }
     return head;
 }
 
+// find pair with given sum in DLL
 
-  
 
-int main(){
-    vector<int> arr = {2,3,4,5,3,6,7,3};
-    Node* head = convertArray2DLL(arr);
+Node* findTail(Node* head){
+    Node* tail = head;
+    while(tail->next != NULL){
+        tail = tail->next;
+    }
+    return tail;
+}
+
+vector<pair<int,int>> findpair(Node* head, int k){
+
+    vector<pair<int,int>> ans;
+    if(head == NULL) return ans;
+
+    Node* left = head;
+    Node* right = findTail(head);
+
+    while(left->data <right->data){
+
+        int sum = left->data + right->data;
+
+        if(sum == k){
+            ans.push_back({left->data, right->data});
+            left = left->next;
+            right = right->back;
+        }
+        else if(sum < k){
+            left = left->next;
+        }
+        else{
+            right = right->back;
+        }
+    }
+
+    return ans;
+}
+
+// remove duplicate from the sorted array
+
+Node* deleteduplicate(Node*head){
+    Node* temp = head;
+    while(temp!=NULL && temp->next!=NULL){
+        Node* nextNode = temp->next;
+        while(nextNode!=NULL && nextNode->data==temp->data){
+            Node*duplicate= nextNode;
+            nextNode = nextNode->next;
+            delete(duplicate);
+        }
+        temp->next = nextNode;
+        if(nextNode!=NULL) nextNode->back = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main()
+{
+    // vector<int> arr = {2, 1, 4, 3, 6, 7, 3};
+    // vector<int> arr = {1,2,3,4,9}; // for pair sum problem
+    vector<int> arr = {1,1,1,2,2,3,3};
+    Node *head = convertArray2DLL(arr);
     // printDLL(head);
     // head = deletehead(head);
 
@@ -253,9 +352,18 @@ int main(){
 
     //  head = reverseDLL(head);
 
-   
     //  head = reverseDLL2(head);
-   int key = 3;
-     head = deleteoccurance(head , key);
+    //    int key = 3;
+    //      head = deleteoccurance(head , key);
+    // vector<pair<int,int>> result = findpair(head, 5);
+    
+    // cout << "Pairs with sum 5: ";
+    // for(auto &p : result){
+    //     cout << "(" << p.first << "," << p.second << ") ";
+    // }
+    // cout << endl
+
+    head = deleteduplicate(head);
+
     printDLL(head);
 }
