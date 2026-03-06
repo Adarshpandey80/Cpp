c92a2ab81fa7fe38bc935bfc0f8841f5a96071be
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-03-06 22:56:22 +0530

    Balanced Parenthesis

--- DSA/OOPS/LinearDS/Stackclass.cpp
+++ DSA/OOPS/LinearDS/Stackclass.cpp
@@ -67,6 +67,28 @@ public:
     }
 };
 
+// Balanced Parenthesis using stack
+
+bool isBalanced(string s){
+    stack<char> st;
+    for(int i=0;i<s.length();i++){
+        char ch = s[i];
+        if(ch=='('||ch=='{'||ch=='['){
+            st.push(ch);
+        }else {
+            if(st.empty()){
+                return false;
+            }
+            char top = st.top();
+            st.pop();
+            if((ch==')'&&top!='(')||(ch=='}'&&top!='{')||(ch==']'&&top!='[')){
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+
 int main(){
     push(10);
     push(20);
