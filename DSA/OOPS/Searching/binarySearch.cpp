984fd2d8045dfe8073c6ec8e84b82a573d2f74ab
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-03-17 23:16:41 +0530

    KOKO eating Bananas

--- DSA/OOPS/Searching/binarySearch.cpp
+++ DSA/OOPS/Searching/binarySearch.cpp
@@ -666,6 +666,40 @@ int NthRoot(int n , int m){
     return -1;
 }
 
+// KOKO eating Bananas 
+
+int findMax(vector<int> &v){
+    int mx = INT_MIN;
+    for(int i=1;i<v.size();i++){
+        mx = max(mx , v[i]);
+    }
+    return mx;
+}
+
+int calculateTotalHours(vector<int> &v , int hourly){
+    int totalH =0;
+    for(int i=0;i<v.size();i++){
+        totalH += ceil((double)v[i]/(double)hourly); // Calculate hours needed to eat all bananas at speed hourly
+    }
+    return totalH;
+}
+
+int miniRateToEatBananas(vector<int> &v , int h){ 
+    int low =1 ,  high = findMax(v);
+    while(low<=high){
+         int mid = (low+high)/2;
+        int totalH = calculateTotalHours(v, mid);
+       
+        if(totalH <= h){
+            high = mid - 1;
+        } else {
+            low = mid + 1;  
+    }
+   
+}
+ return low; // Return the minimum eating speed that allows Koko to finish all bananas within h hours
+}
+ 
 
 int main()
 {
