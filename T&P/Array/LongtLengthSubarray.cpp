691a7d471125367ceb2c59de712e44bdbdb019bf
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-03-28 22:10:54 +0530

    `Added new function longestSubarrayM2 using set data structure`

--- T&P/Array/LongtLengthSubarray.cpp
+++ T&P/Array/LongtLengthSubarray.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include<algorithm>
+#include<set>
 using namespace std;
 
 // find longest subarray with same element
@@ -18,8 +19,29 @@ int longestSubarray(vector<int> &arr , int n){
     return maxi;
 }
 
+// using set
+int longestSubarrayM2(vector<int> &arr , int n){
+    set<int> st;
+    int count = 1;
+    int maxi = 1;
+    for(int i = 0;i<n;i++){
+        if(st.find(arr[i]) == st.end()){
+            st.insert(arr[i]);
+            count++;
+        } else {
+            st.clear();
+            st.insert(arr[i]);
+            count = 1;
+        }
+        maxi = max(maxi , count);
+    }
+    return maxi;
+}
+
 int main(){
     vector<int> arr= {1,2,4,4,6,7,6};
     int n = arr.size();
     cout<<" longest subArray "<<longestSubarray(arr,n);
+    cout<<" longest subArray M2 "<<longestSubarrayM2(arr,n);
+
 }
