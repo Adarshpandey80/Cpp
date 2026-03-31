d1f75f968b11fda1c493017e6f28ec8b982ba170
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-03-31 22:39:19 +0530

    `Added code for 2D array spiral order `

--- array.cpp
+++ array.cpp
@@ -496,6 +496,8 @@ int main(){
 
 
 // 2D arraya  spiral order
+
+
 // class Solution {
 // public:
 //     vector<int> spiralOrder(vector<vector<int>>& matrix) {
@@ -531,3 +533,19 @@ int main(){
 //     }
 // };   
 
+// 2D arraya  diagonal sum
+
+ class diagnoalsum {
+    public:
+        int diagonalSum(vector<vector<int>>& mat) {
+            int n = mat.size();
+            int sum = 0;
+            for(int i=0;i<n;i++){
+                sum += mat[i][i]; // primary diagonal
+                if(i != n - i - 1){
+                    sum += mat[i][n - i - 1]; // secondary diagonal
+                }
+            }
+            return sum;
+        }
+ };
