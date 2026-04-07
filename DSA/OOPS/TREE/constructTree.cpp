32d40c01334d6d84012451bc177d2c9e9926aa85
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-07 22:28:37 +0530

    Add implementation for constructing a binary tree

---
+++ DSA/OOPS/TREE/constructTree.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+ struct Node{
+    int data ; 
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main(){
+   struct Node* root = new Node(1);
+   root->left = new Node(3);
+   root->right = new Node(4);
+   root->left->right = new Node(5);
+}
