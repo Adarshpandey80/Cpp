0aafd322a8791634d2a4d01363775e8d0790cbc2
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-07 23:17:45 +0530

    traverse tree

--- DSA/OOPS/TREE/constructTree.cpp
+++ DSA/OOPS/TREE/constructTree.cpp
@@ -8,12 +8,41 @@ using namespace std;
     Node(int val){
         data = val;
         left = right = NULL;
+    } 
+
+    void preorder(Node* root){
+        if(root == NULL) return;
+        cout<<root->data;
+        preorder(root->left);
+        preorder(root->right);
+       
     }
+
+    void postorder(Node* root){
+        if(root==NULL) return;
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data;
+    }
+
+    void inorder(Node* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        cout<<root->data;
+        inorder(root->right);
+    }
+
+
 };
 
 int main(){
-   struct Node* root = new Node(1);
+    Node* root = new Node(1);
    root->left = new Node(3);
    root->right = new Node(4);
    root->left->right = new Node(5);
+
+   Node* temp;
+//    temp->preorder(root);
+//    temp->postorder(root);
+       temp->inorder(root);
 }
