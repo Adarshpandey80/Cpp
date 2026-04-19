f11730ce24f38d0d629b9a8c3589987c3ed898af
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-19 16:40:56 +0530

    add matrix Add, Sub, Mul, transpose

--- AmarSir/Matrix.cpp
+++ AmarSir/Matrix.cpp
@@ -2,24 +2,84 @@
 using namespace std;
 
 class MatrixOp{
-   public:
-void matrixInsert(int m , int n){
-        int arr[m][n];
-       for(int i = 0;i<m;i++){
-              for(int j =0 ;j<n;j++){
-                   cout<<"Enter the "<<i<<j<<" : ";
-                   cin>>arr[i][j];
-              }
-          }
-     }
- 
+public:
+
+void matrixInsert(int arr[3][3], int m, int n , int arr2[3][3]){
+
+    bool an = true;
+
+    while (an){
+        string ans;
+        cout<<"\nTraverse (t) Exit (e) addition (a) multiplication (m) transpose (tr): ";
+        cin>>ans;
+
+        // Traverse
+        if(ans == "t"){
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    cout << arr[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        // Exit
+        else if(ans == "e"){
+            break;
+        }
+
+        // Addition
+        else if(ans == "a"){
+            int sum[3][3];
+
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    sum[i][j] = arr[i][j] + arr2[i][j];
+                    cout << sum[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        // Multiplication
+        else if(ans == "m"){
+            int mul[3][3] = {0};
+
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    for(int k = 0; k < n; k++){
+                        mul[i][j] += arr[i][k] * arr2[k][j];
+                    }
+                    cout << mul[i][j] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        // Transpose
+        else if(ans == "tr"){
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    cout << arr[j][i] << " ";
+                }
+                cout << endl;
+            }
+        }
+
+        else{
+            cout<<"Invalid input"<<endl;
+        }
+    }
+}
 };
 
 int main(){
-      int m , n;
-      cout<<"Enter the size of the array m&n : ";
-     cin>>m>>n;
-     MatrixOp obj;
-    obj.matrixInsert(m,n);
-}
 
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int arr2[3][3] = {{9,8,7},{6,5,4},{3,2,1}};
+
+    MatrixOp obj;
+    obj.matrixInsert(arr , 3 , 3 , arr2);
+
+    return 0;
+}
