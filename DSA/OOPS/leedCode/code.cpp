74e14234681dc7e8662b4f240c140a869addd6ad
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-26 23:17:01 +0530

    maxSubArray

--- DSA/OOPS/leedCode/code.cpp
+++ DSA/OOPS/leedCode/code.cpp
@@ -65,3 +65,14 @@ Node* reverseLL(Node* head){
     return prev;
 }
 
+int maxSubArray(vector<int>& arr){
+    int sum = 0, maxi = INT_MIN;
+
+    for(int i = 0; i < arr.size(); i++){
+        sum += arr[i];
+        maxi = max(maxi, sum);
+
+        if(sum < 0) sum = 0;
+    }
+    return maxi;
+}
