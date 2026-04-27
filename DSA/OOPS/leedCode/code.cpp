210487c646d94f1a3d4bf7c9e05561e9fd5e0d83
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-27 23:31:29 +0530

    binarySearch

--- DSA/OOPS/leedCode/code.cpp
+++ DSA/OOPS/leedCode/code.cpp
@@ -76,3 +76,17 @@ int maxSubArray(vector<int>& arr){
     }
     return maxi;
 }
+
+
+int binarySearch(vector<int>& arr, int target){
+    int low = 0, high = arr.size()-1;
+
+    while(low <= high){
+        int mid = (low + high)/2;
+
+        if(arr[mid] == target) return mid;
+        else if(arr[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
