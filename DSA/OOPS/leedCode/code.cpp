8e589843a35e330e4a7761ded022a338643fc8fe
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-27 23:34:55 +0530

    countSubarrays

--- DSA/OOPS/leedCode/code.cpp
+++ DSA/OOPS/leedCode/code.cpp
@@ -90,3 +90,22 @@ int binarySearch(vector<int>& arr, int target){
     }
     return -1;
 }
+
+
+int countSubarrays(vector<int>& arr, int k){
+    unordered_map<int,int> mp;
+    int sum = 0, count = 0;
+
+    mp[0] = 1;
+
+    for(int i = 0; i < arr.size(); i++){
+        sum += arr[i];
+
+        if(mp.find(sum - k) != mp.end()){
+            count += mp[sum - k];
+        }
+
+        mp[sum]++;
+    }
+    return count;
+}
