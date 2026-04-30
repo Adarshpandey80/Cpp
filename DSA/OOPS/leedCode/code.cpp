27a3087674b6cd53b24d7132cf9d7b644c62c152
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-04-30 22:19:05 +0530

    merge

--- DSA/OOPS/leedCode/code.cpp
+++ DSA/OOPS/leedCode/code.cpp
@@ -156,15 +156,30 @@ int maxSubArray(vector<int>& arr){
 // }
 
 
-bool hasCycle(Node* head){
-    Node* slow = head;
-    Node* fast = head;
+// bool hasCycle(Node* head){
+//     Node* slow = head;
+//     Node* fast = head;
 
-    while(fast && fast->next){
-        slow = slow->next;
-        fast = fast->next->next;
+//     while(fast && fast->next){
+//         slow = slow->next;
+//         fast = fast->next->next;
 
-        if(slow == fast) return true;
+//         if(slow == fast) return true;
+//     }
+//     return false;
+// }
+
+vector<int> merge(vector<int>& a, vector<int>& b){
+    vector<int> ans;
+    int i = 0, j = 0;
+
+    while(i < a.size() && j < b.size()){
+        if(a[i] < b[j]) ans.push_back(a[i++]);
+        else ans.push_back(b[j++]);
     }
-    return false;
+
+    while(i < a.size()) ans.push_back(a[i++]);
+    while(j < b.size()) ans.push_back(b[j++]);
+
+    return ans;
 }
