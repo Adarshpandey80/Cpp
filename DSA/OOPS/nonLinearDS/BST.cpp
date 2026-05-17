9e8bc54c9df70cc7b7c5d679ad0203fa2fe1216e
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-05-17 23:37:58 +0530

    countNodesAtLevel

--- DSA/OOPS/nonLinearDS/BST.cpp
+++ DSA/OOPS/nonLinearDS/BST.cpp
@@ -140,6 +140,17 @@ class Tree{
         }
         return countNonLeafNodes(root->left)+countNonLeafNodes(root->right)+1;
     }
+
+    // Function to find the number of nodes at a given level in BST
+    int countNodesAtLevel(Tree *root,int level){
+        if(root==NULL){
+            return 0;
+        }
+        if(level==0){           
+            return 1;
+        }
+        return countNodesAtLevel(root->left,level-1)+countNodesAtLevel(root->right,level-1);
+    }   
     
 };
 
