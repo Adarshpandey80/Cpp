fcf9c616fa57e8bddb4c6a29dd9ce9e52645e454
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-05-22 22:50:53 +0530

    Rotate array X times

---
+++ TCS NQT/RotareXtimesarray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+class Solution {
+  public:
+   void reverse(vector<int> &arr , int start, int end){
+           while(start<end){
+               swap(arr[start] , arr[end]);
+               start++;
+               end--;
+           }
+       }
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+        int n = arr.size();
+         d = d%n;
+         reverse(arr, 0 , d-1);
+         
+         reverse(arr,d,n-1);
+         reverse(arr,0,n-1);
+       
+        
+    }
+};
