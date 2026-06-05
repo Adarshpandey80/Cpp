63b43a41be0dc9a9cfa883637bcf398855e0194e
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-06-05 22:08:56 +0530

    greater than all the element to their right side

--- DSA/DSAarray.cpp
+++ DSA/DSAarray.cpp
@@ -647,3 +647,25 @@ int main(){
      cout<<ans[i]<<" ";
     }   
 }
+
+// wap to element which are greater than all the element to their right side in the array
+ #include <iostream>
+#include<vector>
+#include<algorithm>     
+using namespace std;
+int main(){
+    vector<int> arr = {16,17,4,3,5,2};
+    int n = arr.size();
+    vector<int> ans;
+    int maxi = INT_MIN;
+    for(int i = n-1;i>=0;i--){
+        if(arr[i]> maxi){
+            ans.push_back(arr[i]);
+        }
+        maxi = max(maxi , arr[i]);
+    }
+   sort(ans.begin() , ans.end());       
+    for(int i = 0 ;i<ans.size() ; i++){
+     cout<<ans[i]<<" ";
+    }
+}   
