fcba07bd0a163d761c02e7d1e6515cd4388ec17d
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-06-09 23:56:26 +0530

    length of the loop in ll

--- DSA/LinkList.cpp
+++ DSA/LinkList.cpp
@@ -786,6 +786,25 @@ void printlist(Node *head)
         }
         return 0;
     }
+  // length of the loop in ll
+    int lengthofloop(Node* head){
+        if(head == NULL) return 0;
+        Node* slow = head;
+        Node* fast = head;
+        while(fast!= NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                int cnt = 1;
+                fast = fast->next;
+                while(slow != fast){
+                    cnt++;
+                    fast = fast->next;
+                }
+                return cnt;
+            }
+        }
+        return 0;
 } 
 
 
