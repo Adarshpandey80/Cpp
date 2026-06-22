f5262494363d7829204112fa8b202ba615e1edf7
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2026-06-22 23:30:35 +0530

    intersectionpoint

--- DSA/LinkList.cpp
+++ DSA/LinkList.cpp
@@ -829,6 +829,18 @@ void printlist(Node *head)
         return NULL;    
     }
 
+    // find the intersection point of two linkedlist
+    Node* intersectionpoint(Node* head1 , Node* head2){
+        if(head1 == NULL || head2 == NULL) return NULL;
+        Node* temp1 = head1;        
+        Node* temp2 = head2;
+        while(temp1 != temp2){
+            temp1 = (temp1 == NULL) ? head2 : temp1->next;
+            temp2 = (temp2 == NULL) ? head1 : temp2->next;      
+    }
+        return temp1;
+    }
+
 int main()
 {
     vector<int> arr = {3, 1, 4, 6, 2};
